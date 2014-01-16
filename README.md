39fbc016ef9e761e8e7e648fe9a265540111f195
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2014-01-16 22:14:42 +0000

    generated module template and modified source file

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-qantic-angularjs-stylemodel
-===========================
+# qantic.angularjs.stylemodel
 
-AngularJS Style Model, can be used to bind style changes to mouse movement (eg. resize, move)
+
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/ntrp/jquery-qantic.angularjs.stylemodel/master/dist/angular-qantic.angularjs.stylemodel.min.js
+[max]: https://raw.github.com/ntrp/jquery-qantic.angularjs.stylemodel/master/dist/angular-qantic.angularjs.stylemodel.js
+
+In your web page:
+
+```html
+<script src="angular.js"></script>
+<script src="dist/qantic.angularjs.stylemodel.min.js"></script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
---
+++ dist/qantic.angularjs.stylemodel.js
@@ -0,0 +1,86 @@
+'use strict';
+angular.module('qantic.angularjs.stylemodel', []).provider('StyleModelContainer', function () {
+  this.$get = function () {
+    var modelTemplate = {
+        minx: 0,
+        maxx: 0,
+        miny: 0,
+        maxy: 0,
+        pivotx: 0,
+        pivoty: 0,
+        styleObject: {},
+        addStyle: function (styleName, transformFn) {
+          this.styleObject[styleName] = {
+            style: transformFn.call(null, this.pivotx, this.pivoty),
+            modelFn: transformFn
+          };
+        },
+        delStyle: function (i) {
+          this.styleObject.splice(i, 1);
+        },
+        transform: function (px, py) {
+          this.pivotx = px = Math.min(Math.max(px, this.minx), this.maxx);
+          this.pivoty = py = Math.min(Math.max(py, this.miny), this.maxy);
+          angular.forEach(this.styleObject, function (value, key) {
+            value['style'] = value['modelFn'].call(null, px, py);
+          });
+        }
+      };
+    var modelsObj = {};
+    return {
+      add: function (name, minx, maxx, miny, maxy, px, py) {
+        var model = angular.copy(modelTemplate);
+        angular.extend(model, {
+          'minx': minx,
+          'maxx': maxx,
+          'miny': miny,
+          'maxy': maxy,
+          'pivotx': px,
+          'pivoty': py
+        });
+        modelsObj[name] = model;
+      },
+      del: function (name) {
+        delete modelsObj[name];
+      },
+      get: function (name) {
+        return modelsObj[name];
+      }
+    };
+  };
+}).directive('transformMultiStyle', [
+  '$document',
+  'StyleModelContainer',
+  function ($document, StyleModelContainer) {
+    return {
+      restrict: 'A',
+      scope: true,
+      link: function (scope, elem, attr) {
+        var styleModel = StyleModelContainer.get(attr.styleName), px = 0, py = 0, dx = 0, dy = 0;
+        elem.bind('mousedown', function () {
+          if (event.button !== 0) {
+            return;
+          }
+          event.preventDefault();
+          $document.unbind('mousemove', trasformStyle);
+          $document.unbind('mouseup', detachTransform);
+          px = styleModel.pivotx;
+          py = styleModel.pivoty;
+          dx = event.pageX - px;
+          dy = event.pageY - py;
+          $document.bind('mousemove', trasformStyle);
+          $document.bind('mouseup', detachTransform);
+        });
+        function trasformStyle(event) {
+          px = event.pageX - dx;
+          py = event.pageY - dy;
+          scope.$apply(styleModel.transform(px, py));
+        }
+        function detachTransform() {
+          $document.unbind('mousemove', trasformStyle);
+          $document.unbind('mouseup', detachTransform);
+        }
+      }
+    };
+  }
+]);
